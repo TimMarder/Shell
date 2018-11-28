736b01084abb9d1525cf1b502a1c54e47f1ccb13
TimMarder <tmarder@stuy.edu>
2018-11-27 20:47:06 -0500

    readline test

--- shell.c
+++ shell.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/wait.h>
+#define LSH_RL_BUFSIZE 1024
 
 //first separate input with the " " delimiter
 //then make the first index the command
@@ -38,20 +39,58 @@ void sep_cmd_arg(char **line, char *cmd, char *args){
   }
 }
 
+//TEST
+char *lsh_read_line(void)
+{
+  int bufsize = LSH_RL_BUFSIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char) * bufsize);
+  int c;
+
+  if (!buffer) {
+    fprintf(stderr, "lsh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  while (1) {
+    // Read a character
+    c = getchar();
+
+    // If we hit EOF, replace it with a null character and return.
+    if (c == EOF || c == '\n') {
+      buffer[position] = '\0';
+      return buffer;
+    } else {
+      buffer[position] = c;
+    }
+    position++;
+
+    // If we have exceeded the buffer, reallocate.
+    if (position >= bufsize) {
+      bufsize += LSH_RL_BUFSIZE;
+      buffer = realloc(buffer, bufsize);
+      if (!buffer) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
+}
+
 int main(int argc, char *argv[]){
 
   char input[256];
-  char newinput[256];
+  //char newinput[256];
   char cmd[256];
   char args[256];
 
   fgets(input, 256, stdin);
 
-  for (int i = 0 ; i < sizeof(input) - 2 ; i++) {
+  /*for (int i = 0 ; i < sizeof(input) - 2 ; i++) {
       newinput[i] = input[i];
-  }
+  }*/
 
-  char **parsed = parse(newinput);
+  char **parsed = parse(input);
   printf("%s %s %s\n", parsed[0], parsed[1], parsed[2]);
 
   //printf("%d\n", strncmp("-a", "-", 1));
