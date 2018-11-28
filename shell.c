d8a3cabc0ce258685b20c1653667dc6b9796dd44
TimMarder <tmarder@stuy.edu>
2018-11-27 20:47:36 -0500

    removed test

--- shell.c
+++ shell.c
@@ -4,7 +4,6 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/wait.h>
-#define LSH_RL_BUFSIZE 1024
 
 //first separate input with the " " delimiter
 //then make the first index the command
@@ -35,44 +34,6 @@ void sep_cmd_arg(char **line, char *cmd, char *args){
     else{
       strncat(args, line[i], 256);
       printf("args added: %s\n", args);
-    }
-  }
-}
-
-//TEST
-char *lsh_read_line(void)
-{
-  int bufsize = LSH_RL_BUFSIZE;
-  int position = 0;
-  char *buffer = malloc(sizeof(char) * bufsize);
-  int c;
-
-  if (!buffer) {
-    fprintf(stderr, "lsh: allocation error\n");
-    exit(EXIT_FAILURE);
-  }
-
-  while (1) {
-    // Read a character
-    c = getchar();
-
-    // If we hit EOF, replace it with a null character and return.
-    if (c == EOF || c == '\n') {
-      buffer[position] = '\0';
-      return buffer;
-    } else {
-      buffer[position] = c;
-    }
-    position++;
-
-    // If we have exceeded the buffer, reallocate.
-    if (position >= bufsize) {
-      bufsize += LSH_RL_BUFSIZE;
-      buffer = realloc(buffer, bufsize);
-      if (!buffer) {
-        fprintf(stderr, "lsh: allocation error\n");
-        exit(EXIT_FAILURE);
-      }
     }
   }
 }
